c00a91b595d7251fc5f58cc459b2cc6500dca6f9
Angela-de <you@example.com>
2019-07-12 16:00:39 +0000

    GPIO

---
+++ gpo.py
@@ -0,0 +1,23 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+led1 = LED(27)
+led2 = LED(10)
+
+while True:
+    led.on()
+    sleep(2)
+    led.off()
+    sleep(1)
+    
+    led1.on()
+    sleep(1)
+    led1.off()
+    sleep(1)
+    
+    led2.on()
+    sleep(1)
+    led2.off()
+    sleep(1)
+    
